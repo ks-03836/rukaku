9e6e44fa6c88876eed392f8f4ffe831be96ec12a
ks-03836 <109196559+ks-03836@users.noreply.github.com>
2022-07-13 13:42:08 +0900

    Add files via upload

---
+++ プログラミング/sample/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+$(function () {
+    $('.burger-btn').on('click', function () {
+      $('.burger-btn').toggleClass('close');
+      $('.nav-wrapper').toggleClass('fade');
+      $('body').toggleClass('noscroll');
+    });
+  });
